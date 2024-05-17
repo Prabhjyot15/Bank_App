4dfd70dc8490f176bbff89ca7b925f9df490994e
Janhvi Harwani <janhvi.h@simformsolutions.com>
2024-05-17 15:04:42 -0700

    folder setup updated

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/login") 
-def hello(): 
-    return "Login"
-    
+# Default route   
 @app.route("/") 
 def index(): 
     return "Landing Page!"
 
+# Dummy login route
+@app.route("/login") 
+def hello(): 
+    return "Login"
 
+# Running Server
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
